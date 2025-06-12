3e3185de6113e4c27d0bf478252b12d828991826
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2025-06-12 11:17:04 +0100

    updated api function with use effect and usestate

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,16 +1,22 @@
+import { useState, useEffect } from "react";
 
 function Card(){
-    
-    async function getPokemon(){
-        try {
-            const pokeApi = await fetch(`https://pokeapi.co/api/v2/pokemon`)
-            const pokeData = await pokeApi.json()
+    const [pokemon, setPokemon] = useState(null)
 
-            return pokeData;
-        } catch(err){
-            console.log('Error with API')
+    useEffect(() => {
+        async function getPokemon(){
+            try {
+                const pokeApi = await fetch(`https://pokeapi.co/api/v2/pokemon`)
+                const pokeData = await pokeApi.json()
+
+                setPokemon(pokeData.results[0]);
+            } catch(err){
+                console.log('Error with API')
+            }
         }
-    }
+
+        getPokemon();
+    }, []);
 
     return(
         <div className="container">
