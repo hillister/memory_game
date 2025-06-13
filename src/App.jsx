b503bfcd370f050d06f2838d286a8e9944ac05f8
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2025-06-13 09:34:18 +0100

    moved API into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Card from './Card.jsx'
 
 
 function App() {
-  
+  const [pokemon, setPokemon] = useState(null)
+
+  useEffect(() => {
+      async function getPokemon(){
+          try {
+              const pokeApi = await fetch(`https://pokeapi.co/api/v2/pokemon`)
+              const pokeData = await pokeApi.json()
+              const randomIndex = Math.floor(Math.random() * 20);
+              console.log(randomIndex)
+              setPokemon(pokeData.results[randomIndex]);
+          } catch(err){
+              console.log('Error with API')
+          }
+      }
+
+      getPokemon();
+  }, []);
 
   return (
-    <>
-      <Card />
+    <>  
+      <div className="container">
+        {pokemon ? <Card pokemon={pokemon} /> : <p>Loading...</p>}
+      </div>
     </>
   )
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,34 +1,10 @@
 import { useState, useEffect } from "react";
 
-function Card(){
-    const [pokemon, setPokemon] = useState(null)
-
-    useEffect(() => {
-        async function getPokemon(){
-            try {
-                const pokeApi = await fetch(`https://pokeapi.co/api/v2/pokemon`)
-                const pokeData = await pokeApi.json()
-                const randomIndex = Math.floor(Math.random() * 20);
-                console.log(randomIndex)
-                setPokemon(pokeData.results[randomIndex]);
-            } catch(err){
-                console.log('Error with API')
-            }
-        }
-
-        getPokemon();
-    }, []);
-
+function Card({pokemon}){
     return(
-        <div className="container">
-            {pokemon ? (
-                <>
-                <h3>{pokemon.name}</h3>
-                </>
-            ) : (
-                <p>Loading...</p>
-            )}
-        </div>
+        <>
+            <h3>{pokemon.name}</h3>
+        </>
     )
 }
 
