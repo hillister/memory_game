9c54e3f8927e7a28a09306ddcf473637b377afda
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2025-06-13 09:46:15 +0100

    Imgage of pokemon added

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,29 @@
 import { useState, useEffect } from "react";
 
 function Card({pokemon}){
+    const [pokemonImg, setPokemonImg] = useState(null)
+    useEffect(() => {
+        async function getPokemonImage(){
+            try {
+                const pokeApi = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+                const pokeData = await pokeApi.json()
+                const imgURL = pokeData.sprites.front_default
+                setPokemonImg(imgURL)
+            } catch(err){
+                console.log('Error with API')
+            }
+        }
+  
+        getPokemonImage();
+    }, []);
     return(
         <>
             <h3>{pokemon.name}</h3>
+            {pokemonImg ? (
+                <img src={pokemonImg} alt={pokemon.name} />
+            ) : (
+                <p>Loading image...</p>
+            )}
         </>
     )
 }
